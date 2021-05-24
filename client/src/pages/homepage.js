1128a9cfa1c74119efcef6e6f27396e35ba2ef2f
Yixin Cai <yixin.cai@student.unimelb.edu.au>
2021-05-24 23:57:10 +1000

    Update homepage.js

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -2,6 +2,7 @@ import 'antd/dist/antd.css';
 import '../pages/main.css';
 import LeafletMap  from '../components/LeafletMap.js';
 import Header from '../components/header';
+import Footer from '../components/Footer';
 
 
 export default function CustomerMain(props) {
@@ -13,9 +14,12 @@ export default function CustomerMain(props) {
                     vendors = {props.location.state.vendors}
                     center = {props.location.state.position}
                     path = {props.location.pathname}/>
+                    
             <LeafletMap center = {props.location.state.position}
                         vendors = {props.location.state.vendors}
                         customer = {props.location.state.customer} />
+
+            <Footer/>
         </>
     )
 }   
